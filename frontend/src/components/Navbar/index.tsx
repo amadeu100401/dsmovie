35f4ba21e832c3b0eea79581c897b59777342640
Amadeu Martim <amadeu.souunit.com.br>
2022-01-11 17:46:47 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
     return (<header>
         <nav className="container">
             <div className="dsmovie-nav-content">
-                <h1>DSMovire</h1>
+                <h1>DSMovie</h1>
                 <a href="https://github.com/amadeu100401">
                     <div className='dsmovie-contact-container'>
                         <GitHubIcon />
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form() {
+
+    return (
+        <h1>Página de formulários</h1>
+    )
+
+}
+
+export default Form;
