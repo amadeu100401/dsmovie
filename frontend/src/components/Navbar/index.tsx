84c5e1b7dae2550bf583253b9dd5aa3d585d156f
Amadeu Martim <amadeu.souunit.com.br>
2022-01-14 16:23:49 -0300

    Ajuste

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,11 +6,11 @@ function Navbar() {
     return (<header>
         <nav className="container">
             <div className="dsmovie-nav-content">
-                <h1>DSMovie</h1>
+                <h1>MovieAvaliator</h1>
                 <a href="https://github.com/amadeu100401">
                     <div className='dsmovie-contact-container'>
                         <GitHubIcon />
-                        <p className='dsmovie-contact-link'>/devsuperior</p>
+                        <p className='dsmovie-contact-link'>/AmadeuMartim</p>
                     </div>
                 </a>
             </div>
